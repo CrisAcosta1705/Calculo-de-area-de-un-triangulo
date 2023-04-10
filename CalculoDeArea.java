99fb93b7671b4963e9dac202a0b5fa5133c18517
CrisAcosta1705 <128009007+CrisAcosta1705@users.noreply.github.com>
2023-04-09 23:09:41 -0600

    Add files via upload

---
+++ CalculoDeArea.java
@@ -0,0 +1,9 @@
+package Ejercicio;
+import java.util.Scanner;
+public class CalculoDeArea {
+
+	Scanner tc = new Scanner(System.in);
+	int A, B, H;
+	
+	System.out.pritnln("Ingrese la Altura del ")
+}
